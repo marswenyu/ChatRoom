2d9baf07d6a97c466e53197673570ec40d5ba900
Jay_Shih <Jay_Shih@bb.com>
2017-04-18 17:37:36 +0800

    add longin socket function

--- chat/chatApp.js
+++ chat/chatApp.js
@@ -1,8 +1,68 @@
-var HelloMessage = React.createClass({
-    render: function() {
-    return <div ><input type="text"/><button>login</button></div>;
-  }
-});
+class HelloMessage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: '', users:[], username_is_used:false, is_login:false, messages:[]};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    componentDidMount() {
+		var that = this;
+		this.socket = io();
+        this.handleChange = this.handleChange.bind(this);
+  
+        this.socket.on('userAddingResult', function (comments) {
+            console.log("userAddingResult:"+comments.result);
+			that.setState({ username_is_used: !comments.result });
+            that.setState({is_login:comments.result});
+		});
+  
+        this.socket.on('userAdded', function (data) {
+			
+		});
+
+        this.socket.on('allUser', function(data) {
+            for (var i = 0; i < data.length; i++) { 
+                console.log("userAdded:"+data[i].nickname);
+            }    
+        });
+	}
+
+    userAddingResult(){
+        console.log("onclick:");
+    }
+
+    handleClick(){
+        console.log("onclick:"+this.state.value+this.state.is_login);
+        this.socket.emit("addUser",{nickname:this.state.value});
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    render(){
+        return (
+            !this.state.is_login ? 
+                (<div>
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                    <button onClick={this.handleClick}>
+                        ffe
+                    </button>
+                    {this.state.username_is_used && <p>This Nickname:{this.state.value} is Used</p>}
+                </div>) 
+                : 
+                (<div>
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                    <button onClick={this.handleClick}>
+                        ddd
+                    </button>
+                    {this.state.username_is_used && <p>This Nickname:{this.state.value} is Used</p>}
+                </div>) 
+        );
+    }
+}
  
 ReactDOM.render(
   <HelloMessage />,
